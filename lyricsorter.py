2fc0729db428de76e2d433e026b1c970e46ab4a1
jpakdel <32276929+jpakdel@users.noreply.github.com>
2018-09-02 15:46:00 -0500

    Update lyricsorter.py

--- lyricsorter.py
+++ lyricsorter.py
@@ -10,7 +10,9 @@ dynamodb = boto3.resource("dynamodb")
 proxy_table = dynamodb.Table("Proxy")
 word_table = dynamodb.Table("Word")
 song_table = dynamodb.Table("Song")
-dictionary=PyDictionary()
+dictionary = PyDictionary()
+
+
 def get_lyrics_list():
     """returns list of all the song urls we have scraped
     """
@@ -24,10 +26,12 @@ def get_lyrics_list():
 
 
 song_words = []
+
+
 def get_words(link: str):
     """Uses a song url to look up the lyrics and get all the individual words from them"""
     response = song_table.get_item(
-        Key = {
+        Key={
             'id': link
         }
     )
@@ -39,7 +43,7 @@ def get_words(link: str):
     output_list = []
     print(link)
     for item in lyric_list:
-        #Take  out the punctuation
+        # Take  out the punctuation
         item = item.replace("?", "")
         item = item.replace(",", "")
         item = item.replace(".", " ")
@@ -47,34 +51,40 @@ def get_words(link: str):
         item = item.replace("-", " ")
         item = item.replace("!", "")
         item = item.lower()
-        
-        #We only want the line if it doesn't have numbers, and doesn't have a colon, which means its not actual lyrics
+
+        # We only want the line if it doesn't have numbers, and doesn't have a colon, which means its not actual lyrics
         if item not in output_list and hasNumbers(item) is False and scraper.contains(item, ":") is False:
             output_list.append(remove_paranthases(item).strip())
 
     for item in output_list:
         word_lines = item.split(" ")
-        #we only want sentences with more than three words, because some of the text in the lyrics aren'tactual song lyrics
-        #and we want to avoid adding those
+        # we only want sentences with more than three words, because some of the text in the lyrics aren'tactual song lyrics
+        # and we want to avoid adding those
         if len(word_lines) > 3:
             for word in word_lines:
                 word = str(word).lower()
-                
-                #make sure we aren't adding html code
-                if len(word)>0 and scraper.contains(word, "&") is False:
+
+                # make sure we aren't adding html code
+                if len(word) > 0 and scraper.contains(word, "&") is False:
                     if word[0] == '\'':
                         word = word[1:len(word)]
-                    #replace slang words with their real equivalent
+
+                    # replace slang words with their real equivalent\
+                    word = slang_cleaner.remove_weirdness(word)
+                    word = slang_cleaner.clean_misspellings(word)
                     word = slang_cleaner.clean_slang(word)
-                    
-                    #add it to our list of words in the song
+
+                    # add it to our list of words in the song
                     if word not in song_words and word != ",":
                         song_words.append(word)
 
 
 
+
 slang_words = []
 proper_words = []
+
+
 def separate_words():
     """separates words into slang and not slang"""
     for word in sorted(song_words):
@@ -89,20 +99,12 @@ def separate_words():
     print("Slang")
     print(slang_words)
 
+
 def insert_words():
     """Inserts all the proper words as individual items into the database and slang words into a list"""
     for word in proper_words:
-        response = word_table.get_item(
-            Key={
-                'id': word
-            }
-        )
-        if 'Item' not in response:
-            word_table.put_item(
-                Item={
-                    'id': word
-                }
-            )
+        if len(word)>0:
+            slang_cleaner.insert_word(word)
     response = word_table.get_item(
         Key={
             'id': "slang_words"
@@ -114,6 +116,7 @@ def insert_words():
             dynamo_slang_words.append(word)
     scraper.update_table(word_table, "slang_words", "words", sorted(dynamo_slang_words))
 
+
 def lyric_parse(x: int, y: int):
     """
     Gets the lyrics from multiple songs, puts all their words into a single list, separates them into slang and not slang,
@@ -124,12 +127,15 @@ def lyric_parse(x: int, y: int):
     """
     song_urls = get_song_url_list()
     while x < y:
+        if x > len(song_urls)-1:
+            break
         song_url = song_urls[x]
         get_words(song_url)
-        x +=1
+        x += 1
     separate_words()
     insert_words()
-    
+
+
 def get_song_url_list():
     response = song_table.get_item(
         Key={
@@ -146,8 +152,12 @@ def get_song_url_list():
     for item in map_2:
         song_url_list.append(item)
 
-    return(song_url_list)
+    return (song_url_list)
+
+
 sentences = []
+
+
 def remove_paranthases(input: str):
     output = ""
     found_p = False
@@ -155,11 +165,13 @@ def remove_paranthases(input: str):
         if c == '(':
             found_p = True
         if found_p is False:
-            output+=c
+            output += c
         if c == ")":
             found_p = False
     return output
+
+
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
-#lyric_parse(10, 30)
+#lyric_parse(750, 900)
