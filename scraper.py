490c7b76767cf321323b3497722c8d6edcb0af13
jpakdel <32276929+jpakdel@users.noreply.github.com>
2018-09-24 21:47:32 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,244 +2,180 @@ from bs4 import BeautifulSoup
 import requests
 import boto3
 import random
+import scraper
+import slang_cleaner
+from botocore.exceptions import ClientError
+from PyDictionary import PyDictionary
 
 dynamodb = boto3.resource("dynamodb")
 proxy_table = dynamodb.Table("Proxy")
 word_table = dynamodb.Table("Word")
 song_table = dynamodb.Table("Song")
-def proxy_scrape():
-    """Scrapes sslproxies.org to find proxies to use for other website scraping purposes
-     inserts them into DynamoDB table"""
-    proxy_list = []
-    link = 'https://www.sslproxies.org/'
-    script = requests.get(link)
-    soup = BeautifulSoup(script.content, 'html.parser')
-    try:
-        tds = list(soup.find_all("td", {'class': None}))
-        for index, val in enumerate(tds):
-            if index > 399:
-                break
-            proxy_list_index = int(index/4)
-            current_value = str(tds[index]).replace("<td>", "")
-            current_value = current_value.replace("</td>", "")
-            if index % 4 == 0:
-                proxy_list.append(["",""])
-                proxy_list[proxy_list_index][0] = current_value
-            elif index % 4 == 1:
-                proxy_list[proxy_list_index][1] = current_value
-    except AttributeError:
-        print("Attribute Error")
+dictionary = PyDictionary()
 
-    for index, val in enumerate(proxy_list):
-        proxy_table.put_item(
-            Item={
-                'id': str(index),
-                'ip': proxy_list[index][0],
-                'port': proxy_list[index][1]
-            }
-        )
-    proxy_table.put_item(
-        Item={
-            'id': "num_proxies",
-            'value': str(len(proxy_list))
-        }
-    )
 
-def get_proxy():
+def get_lyrics_list():
+    """returns list of all the song urls we have scraped
     """
-    Looks up a random proxy from DynamoDB table and returns it
-    :returns array of two strings, IP and port:
-    """
-    response = proxy_table.get_item(
-        Key={
-            'id': "num_proxies"
-        }
-    )
-    item = response['Item']
-    num_proxies = int(item['value'])
-    choice = random.randint(0, num_proxies-1)
-    response = proxy_table.get_item(
-        Key={
-            'id': str(choice)
-        }
-    )
-    item = response['Item']
-    proxy_response = "http://" + str(item["ip"]) + ":" + item["port"] + "/"
-    #proxy_response = [item["ip"], item["port"]]
-    return proxy_response
-
-def update_table(table, id: str, key: str, value: str):
-    """Updates user table, just for saving space.
-    Args: table is the table we are updating, id is the id of the item we are updating, key is the attribute name, and
-    value is the attribute value.
-    """
-    table.update_item(
-        Key={
-            'id': id,
-        },
-        UpdateExpression='SET ' + key + ' = :val1',
-        ExpressionAttributeValues={
-            ':val1': value
-        }
-    )
-    return
-
-word_dict = []
-
-def insert_song(link, lyrics):
-    """First we update the number of items in the table, then we add an index for which number entry the link is,
-    then we finally insert the chorus and verse.
-    :param link: str for the url we are scraping lyrics from, becomes unique identifier for the song lyrics
-    :param lyrics: str array with two parts, chorus and verse
-    :return:
-    """
-    response = song_table.get_item(
-        Key={
-            'id': link
-        }
-    )
-
-    if 'Item' in response:
-        print("{} already exists in song table".format(link))
-        return
-
     response = song_table.get_item(
         Key={
             'id': "map"
         }
     )
     map = response['Item']
+    return list(map["url_list"])[0:5]
+
+
+song_words = []
+
+
+def get_words(link: str):
+    """Uses a song url to look up the lyrics and get all the individual words from them"""
+    response = song_table.get_item(
+        Key={
+            'id': link
+        }
+    )
+
+    item = response['Item']
+    lyrics = str(item['lyrics'])
+    lyric_list = lyrics.split("\n")
+
+    output_list = []
+    output_list2 =[]
+    i = 0
+    print(link)
+    for item in lyric_list:
+        # Take  out the punctuation
+        item = item.replace("?", "")
+        item = item.replace(",", "")
+        item = item.replace(".", " ")
+        item = item.replace("\"", "")
+        item = item.replace("-", " ")
+        item = item.replace("!", "")
+        item = item.lower()
+
+        # We only want the line if it doesn't have numbers, and doesn't have a colon, which means its not actual lyrics
+        if item not in output_list and hasNumbers(item) is False and scraper.contains(item, ":") is False:
+            output_list.append(remove_paranthases(item).strip())
+            output_list2.append([])
+
+    for item in output_list:
+
+        word_lines = item.split(" ")
+        # we only want sentences with more than three words, because some of the text in the lyrics aren'tactual song lyrics
+        # and we want to avoid adding those
+        if len(word_lines) > 3:
+            for word in word_lines:
+                word = str(word).lower()
+
+                # make sure we aren't adding html code
+                if len(word) > 0 and scraper.contains(word, "&") is False:
+                    if word[0] == '\'':
+                        word = word[1:len(word)]
+
+                    # replace slang words with their real equivalent\
+                    word = slang_cleaner.remove_weirdness(word)
+                    word = slang_cleaner.clean_misspellings(word)
+                    word = slang_cleaner.clean_slang(word)
+                    output_list2[i].append(word)
+                    # add it to our list of words in the song
+                    if word not in song_words and word != ",":
+                        song_words.append(word)
+        i += 1
+    try:
+        scraper.update_table(song_table, link, "lyric_array", output_list2)
+    except ClientError:
+        pass
+
+slang_words = []
+proper_words = []
+
+
+def separate_words():
+    """separates words into slang and not slang"""
+    for word in sorted(song_words):
+        slang = slang_cleaner.is_slang(word)
+        if slang:
+            slang_words.append(word)
+        else:
+            proper_words.append(word)
+        print("{} is {}".format(word, str(slang)))
+    print("Words")
+    print(proper_words)
+    print("Slang")
+    print(slang_words)
+
+
+def insert_words():
+    """Inserts all the proper words as individual items into the database and slang words into a list"""
+    for word in proper_words:
+        if len(word)>0:
+            slang_cleaner.insert_word(word)
+    response = word_table.get_item(
+        Key={
+            'id': "slang_words"
+        }
+    )
+    dynamo_slang_words = list(response['Item']['words'])
+    for word in slang_words:
+        if word not in dynamo_slang_words:
+            dynamo_slang_words.append(word)
+    scraper.update_table(word_table, "slang_words", "words", sorted(dynamo_slang_words))
+
+
+def lyric_parse(x: int, y: int):
+    """
+    Gets the lyrics from multiple songs, puts all their words into a single list, separates them into slang and not slang,
+    then puts them into database
+    :param x: index of first song lyric we want to get words from
+    :param y: index of second song lyric we get lyrics from
+    :return:
+    """
+    song_urls = get_song_url_list()
+    while x < y:
+        if x > len(song_urls)-1:
+            break
+        song_url = song_urls[x]
+        get_words(song_url)
+        x += 1
+    #separate_words()
+    #insert_words()
+
+
+def get_song_url_list():
+    response = song_table.get_item(
+        Key={
+            'id': "map"
+        }
+    )
+    song_url_list = list(response['Item']['url_list'])
     response = song_table.get_item(
         Key={
             'id': "map_2"
         }
     )
-    map_2 = response['Item']
+    map_2 = list(response['Item']['url_list'])
+    for item in map_2:
+        song_url_list.append(item)
 
-    try:
-        url_list = list(map['url_list'])
-        num_items = len(url_list) + 1
-        url_list_2 = list(map_2['url_list'])
-        num_items += len(url_list_2) + 1
-        url_list_2.append(link)
-        print("{} is the #{} item in the song table".format(link, str(num_items)))
+    return (song_url_list)
 
-        update_table(song_table, "map_2", "url_list", url_list_2)
+sentences = []
 
-        song_table.put_item(
-            Item={
-                'id': link,
-                'lyrics': lyrics
-            }
-        )
-    except KeyError:
-        print("Key Error")
-    pass
+def remove_paranthases(input: str):
+    output = ""
+    found_p = False
+    for c in input:
+        if c == '(':
+            found_p = True
+        if found_p is False:
+            output += c
+        if c == ")":
+            found_p = False
+    return output
 
-def contains(string: str, snippet: str)-> bool:
-    """check to see if string contains another string"""
-    string = string.lower()
-    #string)
-    snippet= snippet.lower()
-    if len(snippet) == 1:
-        for i in string:
-            if i == snippet:
-                return True
-        return False
-    #print(snippet)
-    for i, char in enumerate(string):
-        if (i + len(snippet))> (len(string)):
-            return False
-        snip = string[i:i+len(snippet)]
-        if snip == snippet:
-            return True
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
 
 
-def az_lyric_scrape(link: str):
-    """
-    This function scrapes definitions from dictionary.com to find the part of speech of a word and inserts it into
-    DynamoDB word table.
-    :param link, url that we scrape:
-    """
-    worked = False
-    while worked is False:
-        try:
-            print("Using proxy")
-            proxy = get_proxy()
-            # proxy = "http://" + "35.233.225.185" + ":8080/"
-            script = requests.get(link, proxies={"https": proxy, })
-            soup = BeautifulSoup(script.content, 'html.parser')
-            worked = True
-        except requests.exceptions.ConnectionError:
-            worked = False
-            pass
-    html_lines = str(soup.prettify())
-    line = ""
-    found_lyrics = False
-    viable_lyric = False
-
-    #used to make sure we don't take the introduction from songs, we only want lyrics
-    intro_counter = 0
-    lyrics= ""
-    for c in html_lines:
-        line+=c
-
-        if c == "\n":
-            #print(line)
-            line = line.strip()
-            print(line)
-            if contains(line, "(") or line[0] == "<" or contains(line, "["):
-                viable_lyric = False
-            else:
-                viable_lyric = True
-            if found_lyrics and viable_lyric and intro_counter > 1 and len(line) > 0:
-                lyrics+=line
-                lyrics += "\n"
-            if contains(line, "</p>") or contains(line, "[Intro"):
-                found_lyrics = False
-            if contains(line, "\"verse\""):
-                intro_counter +=1
-                found_lyrics = True
-
-            line = ""
-    lyrics=lyrics.strip()
-    print(lyrics)
-
-    if len(lyrics)>0:
-        insert_song(link, lyrics)
-
-def az_url_scrape(link: str):
-    """
-    This function scrapes definitions from dictionary.com to find the part of speech of a word and inserts it into
-    DynamoDB word table.
-    :param link, url that we scrape:
-    """
-    worked = False
-    while worked is False:
-        try:
-            print("Using proxy")
-            proxy = get_proxy()
-            #proxy = "http://" + "35.233.225.185" + ":8080/"
-            script = requests.get(link, proxies={"https": proxy,})
-            soup = BeautifulSoup(script.content, 'html.parser')
-            worked = True
-        except requests.exceptions.ConnectionError:
-            worked = False
-            pass
-    html_lines = str(soup.prettify())
-    line = ""
-    found_lyrics = False
-    viable_lyric = False
-
-    #used to make sure we don't take the introduction from songs, we only want lyrics
-    intro_counter = 0
-    url_list = []
-    for c in html_lines:
-        if c == "\n" or c == "\"":
-            if contains(line, ".html") and contains(line, "-lyrics-") and line[len(line) - 4:] == "html" and contains(line, "explicit") is False and contains(line, "remix") is False:
-                url_list.append(line)
-            line = ""
-        else:
-            line += c
-    for item in url_list:
-        az_lyric_scrape(item)
