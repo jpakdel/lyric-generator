afa6865c1528c077f26cad4afa91ca134f0a2d4e
frezafoltran <frezafoltran@wisc.edu>
2018-12-05 00:55:07 -0600

    Update helper_methods.py

--- helper_methods.py
+++ helper_methods.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Aug 25 17:58:02 2018
-
-This file contains auxiliary methods to be further organized into more 
+This file contains auxiliary methods to be further organized into more
 specific folders
 @author: Adm
 """
 
 import boto3
 import random
+import scrapping_methods as sm
+import word_processor as wp
 
-dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
 
 proxy_table = dynamodb.Table("Proxy")
 word_table = dynamodb.Table("Word")
@@ -30,7 +30,7 @@ def get_proxy():
     )
     item = response['Item']
     num_proxies = int(item['value'])
-    choice = random.randint(0, num_proxies-1)
+    choice = random.randint(0, num_proxies - 1)
     response = proxy_table.get_item(
         Key={
             'id': str(choice)
@@ -39,6 +39,7 @@ def get_proxy():
     item = response['Item']
     proxy_response = [item["ip"], item["port"]]
     return proxy_response
+
 
 def update_table(table, id: str, key: str, value: str):
     """Updates user table, just for saving space.
@@ -56,92 +57,159 @@ def update_table(table, id: str, key: str, value: str):
     )
     return
 
-def get_word():
+
+def get_word(table):
     """
     Looks up a random proxy from DynamoDB table and returns it
     :returns array of two strings, IP and port:
     """
-    
-    return list(word_table.scan()['Items'])   
 
-def get_word_relation():
-    """
-    Looks up a random proxy from DynamoDB table and returns it
-    :returns array of two strings, IP and port:
-    """
-    
-    return list(word_relation_table.scan()['Items']) 
+    return list(table.scan()['Items'])
 
-def phonetic_clean(word:str):
-    
+
+def get_by_id(entry_id, table):
+    """This function returns the correspondent entry of an input id in the
+    refered table. It is used in get_rhyme_list for example, in order to get
+    the phonetic of a single word and then compare it to others."""
+
+    response = table.get_item(
+        Key={
+            'id': entry_id
+        }
+    )
+
+    return response['Item']
+
+def phonetic_clean(word: str):
     """
     This function formats the output phonetic by erasing the characters:
-    , "]", ",", "-", " ", ";" from the output 
+    , "]", ",", "-", " ", ";" from the output
     """
-    
+
     initial_length = len(word)
     modified_word = word
     counter = 0
-                        
-    while(counter < initial_length):
-        current = modified_word[counter:counter+1]
-        if (current == "[" or current == "]" or current == " " or 
-            current == "," or current == "-" or current == ";"):
-            modified_word = modified_word[:counter] + modified_word[counter+1:]
+
+    while (counter < initial_length):
+        current = modified_word[counter:counter + 1]
+        if (current == "[" or current == "]" or current == " " or
+                current == "," or current == "-" or current == ";"):
+            modified_word = modified_word[:counter] + modified_word[counter + 1:]
             counter -= 1
-        
+
         counter += 1
     return modified_word
-  
+
+
 def __update_next_stop(raw_phonetic, next_stop, i):
-    
-    """This function is a minor helper function to deal with some exceptions 
-    in the html scrapping. It corrects cases in which the phonetic of a word 
+    """This function is a minor helper function to deal with some exceptions
+    in the html scrapping. It corrects cases in which the phonetic of a word
     has sylables separated by "," and "-" """
-    
+
     next_stop_updated = next_stop
-    next_1 = raw_phonetic[i+2:next_stop-1].find("-")
-    next_2 = raw_phonetic[i+1:next_stop-1].find(",")
-                
+    next_1 = raw_phonetic[i + 2:next_stop - 1].find("-")
+    next_2 = raw_phonetic[i + 1:next_stop - 1].find(",")
+
     next_syllable = min(next_1, next_2)
-                
+
     if next_1 == -1:
         next_syllable = next_2
     if next_2 == -1:
         next_syllable = next_1
-                
+
     if (next_syllable != -1 and raw_phonetic[next_syllable + 3 + i] != "<"):
         raw_phonetic = raw_phonetic[:next_syllable + i + 2] + ">" + raw_phonetic[next_syllable + i + 2:]
         next_stop_updated = next_syllable + i + 2
-        
+
     return [raw_phonetic, next_stop_updated]
 
-def __phonetic_scrape_helper(word:str, word_scraped:str):
 
-    start_index = (word.find(word_scraped),word_scraped.find(word))
-    append_where = 0 
+def __phonetic_scrape_helper(word: str, word_scraped: str):
+    start_index = (word.find(word_scraped), word_scraped.find(word))
+    append_where = 0
     # tells caller where extra_letters must be appended (1 end or 2 start)
 
     l1 = len(word_scraped)
-    
+
     if start_index[0] != -1:
         if start_index[0] == 0:
             extra_letters = word[l1:]
             append_where = 1
         else:
-            extra_letters = word[:start_index[0]] + "-" + word[start_index[0]+l1 :]
+            extra_letters = word[:start_index[0]] + "-" + word[start_index[0] + l1:]
             append_where = 2
-        return (extra_letters,append_where)
-     
+        return (extra_letters, append_where)
+
     if start_index[1] != -1:
         if start_index[1] == 0:
             extra_letters = word_scraped[l1:]
             append_where = 1
         else:
-            extra_letters = word_scraped[:start_index[1]] + "-" + word_scraped[start_index[1]+l1 :]
+            extra_letters = word_scraped[:start_index[1]] + "-" + word_scraped[start_index[1] + l1:]
             append_where = 2
-        return (extra_letters,append_where)
-            
-    if start_index == (-1,-1):
+        return (extra_letters, append_where)
+
+    if start_index == (-1, -1):
         return (-1, append_where)
-    
+
+
+def get_phonetic():
+    """This methods outputs a dictionary with all words and phonetic representation
+    in the database. It will be used to increase the speed in which we find the
+    rhymes/alliteration of a given word"""
+
+    words = get_word(word_table)
+    output = []
+
+    # exclude items with a count number attached
+    # TODO: change range of for loop to extend from 1 to range(len(words)).
+    # Set to 100 for fast testing.
+    for i in range(100):
+        get_id = words[i]["id"]
+        response = word_relation_table.get_item(
+            Key={
+                "id": get_id
+            }
+        )
+        if "Item" in response:
+            print(i)
+            output.append(response["Item"])
+
+    return output
+
+
+def update_phonetics():
+    """This method goes over all entries of word_relation table and checks if each word entry
+     (not containing "_") has a phonetic. If not, it updates the phonetic."""
+
+    entries = wp.find_viable_words()
+    count = 0
+
+    for i in range(len(entries)):
+
+        print(i)
+
+        # Gets the correspondent element in word_relation table
+        try:
+            word_relation_entry = get_by_id(entries[i], word_relation_table)
+
+            if 'phonetic' not in word_relation_entry:
+                new_phon, word = sm.phonetic_scrape(entries[i])
+
+                if new_phon is not -1:
+                    update_table(word_relation_table, entries[i], 'phonetic', '[' + ','.join(new_phon) + ']')
+                    count = count + 1
+
+        except KeyError:
+            new_phon, word = sm.phonetic_scrape(entries[i])
+            if new_phon is not -1:
+
+                count = count + 1
+                word_relation_table.put_item(
+                    Item={
+                        'id': entries[i],
+                        'phonetic': '[' + ','.join(new_phon) + ']',
+                    }
+                )
+
+    return count
