98ccdd727a0683d4be6c9fbd237dc6db8cb38113
jpakdel <32276929+jpakdel@users.noreply.github.com>
2019-03-24 22:32:22 -0500

    Update helper_methods.py

--- helper_methods.py
+++ helper_methods.py
@@ -5,17 +5,110 @@ This file contains auxiliary methods to be further organized into more
 specific folders
 @author: Adm
 """
-
+import json
 import boto3
 import random
-import scrapping_methods as sm
-import word_processor as wp
+#import scrapping_methods as sm
+#import word_processor as wp
 
-
+dynamodb = boto3.resource("dynamodb")
 proxy_table = dynamodb.Table("Proxy")
 word_table = dynamodb.Table("Word")
 song_table = dynamodb.Table("Song")
 word_relation_table = dynamodb.Table("WordRelation")
+debug = False
+
+with open("rhymes\\master_rhymes.json") as f:
+    confirmed_rhymes = json.load(f)
+def jprint(dic):
+    print(json.dumps(dic, indent=2))
+
+def get_song_url_list():
+    """
+    returns list of all the song urls in our database, which are the unique identifiers we use to track their lyrics
+    :return:
+    """
+    response = song_table.get_item(
+        Key={
+            'id': "mapmap"
+        }
+    )
+    maps = list(response['Item']['map_list'])
+    song_url_list = []
+    for map in maps:
+        response = song_table.get_item(
+            Key={
+                'id': map
+            }
+        )
+        song_url_list.extend(list(response['Item']['url_list']))
+
+    return (song_url_list)
+
+def get_all_sentence_array():
+    #I have this option set to true because I already saved it in JSON
+    option_json = True
+    if option_json:
+        with open('rap_sentences_array.json', 'r') as f:
+            corpus = json.load(f)
+            return corpus['value']
+    song_urls = get_song_url_list()
+    sentences = []
+
+    #This is the part of the code that executed once and saved it as a variable
+    for i, link in enumerate(song_urls):
+        response = song_table.get_item(
+            Key={
+                'id': link
+            }
+        )
+        lyrics = []
+        try:
+            lyrics = response['Item']['lyric_array']
+        except KeyError:
+            pass
+        for line in lyrics:
+            if len(line)>2 and len(line)<13:
+                sentences.append(line)
+        #new song marker
+        sentences.append([['']])
+        print(i)
+        print("")
+
+    sent_array = {
+        "value": sentences
+    }
+    with open('rap_sentences_array.json', 'w') as outfile:
+        json.dump(sent_array, outfile, indent=4)
+    pass
+    return sentences
+
+def word_relation_lookup(label):
+    #print(label)
+    file_path = "word_relation/" + label +".json"
+    try:
+
+        with open(file_path) as f:
+            data = json.load(f)
+            data = data["data"]
+            #print("file found")
+    except FileNotFoundError:
+        #print("file not found")
+        response = word_relation_table.get_item(
+            Key={
+                'id': label
+            }
+        )
+        data = response['Item']['words']
+        out_data = {}
+        for item in data:
+            out_data[item] = int(data[item])
+
+        out_data = {"data": out_data}
+        with open(file_path, 'w') as outfile:
+            json.dump(out_data, outfile, indent=4)
+
+    return data
 
 
 def get_proxy():
@@ -177,6 +270,35 @@ def get_phonetic():
 
     return output
 
+def find_viable_words():
+    """
+    Returns list and dict of words we have found more than one time in song lyrics
+    """
+    with open("viable_words.json") as f:
+        viableset = json.load(f)["words"]
+
+    return(viableset)
+
+def get_all_phonetic_array():
+    # I have this option set to true because I already saved it in JSON
+    option_json = True
+    if option_json:
+        with open('rap_phonetic_array.json', 'r') as f:
+            corpus = json.load(f)
+            return corpus
+
+all_phonetics = get_all_phonetic_array()
+
+def check_phonetic_existance(word):
+    """ Checks to see if we have a phonetic representation for word"""
+    if word not in all_phonetics:
+        return False
+    return True
+
+def dprint(word):
+    global debug
+    if debug:
+        print(word)
 
 def update_phonetics():
     """This method goes over all entries of word_relation table and checks if each word entry
@@ -213,3 +335,16 @@ def update_phonetics():
                 )
 
     return count
+
+def get_rhymes(word):
+    global confirmed_rhymes
+    rhymes = confirmed_rhymes[word].keys()
+    output = [[],[]]
+    for item in rhymes:
+        output[0].append(item)
+    with open ("rhymes\\" + word + ".json") as f:
+        data = json.load(f)
+
+    for item in data:
+        output[1].append(item)
+    return output
